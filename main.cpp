cb8ddfc70d1d6ac44f97f0de89ba5e34f26e711b
Ruykaji <ilya.tanc@gmail.com>
2023-09-26 01:14:00 +0300

    Add external libraries: def and lef

--- main.cpp
+++ main.cpp
@@ -10,19 +10,18 @@ int main(int argc, char* argv[])
     if (isInit) {
         std::cerr << "Error: cant't initialize reader!\n"
                   << std::flush;
-        return 0;
+        return 2;
     }
 
     auto file = fopen("/home/alaie/projects/layout-viewer/__tests__/complete.5.8.def", "r");
 
     if (file != nullptr) {
         auto isRead = defrRead(file, "/home/alaie/projects/layout-viewer/layout-viewer/complete.5.8.def", nullptr, 0);
-        auto data = defrGetUserData();
 
         if (isRead) {
             std::cerr << "Error: could not read the file!\n"
                       << std::flush;
-            return 0;
+            return 2;
         }
     }
 
